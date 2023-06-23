2a83b7bb74fb6cf3434c39379de1f2ee93a15ed8
Martín Alturria <104878339+martinalturria@users.noreply.github.com>
2023-06-23 11:43:23 -0300

    Add Architecture, Functionality, Landing, dependencies. (#2)
    * I add the main skeleton of Flutter
    
    Start the Flutter project for the Silver Express app
    
    * Add files .gitattributes and .gitignore
    
    * Add dependencies and sources to the pub.
    
    modify the pubspec file to add font types, the following dependencies are also added:
    -auth0_flutter: 1.2.0
    -google_fonts: ^4.0.3
    -http: ^0.13.4
    -flutter_custom_clippers: ^2.0.0
    -flutter_vector_icons: ^1.0.0
    
    * add folder image and assets/font.
    
    Add folders to handle the different types of media for the project
    
    * Add Landing view and main.dart functionality
    
    modify the main.dart file, add my_app.dart and LandingScreen.
    
    * modifies README.MD with instruction to compile in web.
    
    * Add Screen of the main page and Widgets for dropdown menu
    
    I add the main screen by making the drop-down menu conditioning it by whether it is web or mobile with the screen size.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # silver-express-app
 Silver Express is a taxicab company, this repository save our App.
+
+### 🖥️ Web
+
+```sh
+flutter run -d chrome --web-port 3000 --web-renderer html
+```
---
+++ lib/Screens/LandingScreen/landing_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:silver_express_app/Screens/PrincipalScreen/principal_screen.dart';
+
+class LandingScreen extends StatelessWidget {
+  final Future<void> Function()? login;
+
+  const LandingScreen({Key? key, required this.login}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Color.fromARGB(233, 353, 452, 342), Colors.black45]),
+          ),
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: OvalBottomBorderClipper(),
+                child: Container(
+                    color: Colors.white,
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width *
+                                  0.8, // Ajusta el valor según sea necesario
+                              child: const Text(
+                                'Solicite su Servicio de Taxi con nosotros',
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 40,
+                                  fontFamily: 'Pacifico',
+                                ),
+                                textAlign: TextAlign.center,
+                                overflow: TextOverflow.visible,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(const Size(270, 50)),
+                      backgroundColor: const MaterialStatePropertyAll(
+                          Color.fromARGB(255, 0, 104, 24))),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const PrincipalScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'Comenzemos el Viaje',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  )),
+              const SizedBox(
+                height: 70,
+              ),
+              const Text(
+                '¿Quieres Trabajar con nosotros?',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 20, fontFamily: 'OneDay'),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'CONTACTANOS',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    fontFamily: 'OneDay'),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // Acción al presionar el botón de WhatsApp
+                      // Abre el enlace correspondiente
+                    },
+                    icon: const Icon(
+                      FontAwesome.whatsapp,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Acción al presionar el botón de Facebook
+                      // Abre el enlace correspondiente
+                    },
+                    icon: const Icon(
+                      FontAwesome.facebook,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Acción al presionar el botón de Gmail
+                      // Abre el enlace correspondiente
+                    },
+                    icon: const Icon(
+                      FontAwesome.google,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
