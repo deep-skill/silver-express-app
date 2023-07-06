a26799b6113ff0fea33b0fe942e108696faad927
Roque Molina <roque.nicolas.molina@gmail.com>
2023-07-06 19:06:58 -0300

    Home View

--- lib/presentations/screens/home_screen/home_view.dart
+++ lib/presentations/screens/home_screen/home_view.dart
@@ -1,17 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:silver/domain/entities/enterprise_entity/enterprise_entity.dart';
-import 'package:silver/providers/enterprise/enterprise_providers.dart';
+
+class TripItem {
+  final int tripId;
+  final int reserveId;
+  final String user;
+  final String driver;
+  final double total;
+  final String dateStart;
+  final String dateEnd;
+  final String state;
+  //constructor
+  const TripItem({
+    required this.tripId,
+    required this.reserveId,
+    required this.user,
+    required this.driver,
+    required this.total,
+    required this.dateStart,
+    required this.dateEnd,
+    required this.state,
+  });
+}
+
+const tripItems = <TripItem>[
+  TripItem(
+    tripId: 735292,
+    reserveId: 814837272,
+    user: 'Juan',
+    driver: 'Nahual',
+    total: 83.23,
+    dateStart: '2023/06/22, 17:40 UTC',
+    dateEnd: '2023/06/22, 18:30 UTC',
+    state: 'Active',
+  ),
+  TripItem(
+    tripId: 738164,
+    reserveId: 898873272,
+    user: 'Pedro',
+    driver: 'Carmen',
+    total: 827.43,
+    dateStart: '2023/06/20, 12:00 UTC',
+    dateEnd: '2023/06/20, 13:30 UTC',
+    state: 'Active',
+  ),
+  TripItem(
+    tripId: 733332,
+    reserveId: 814129372,
+    user: 'Carlos',
+    driver: 'Bruno',
+    total: 3.29,
+    dateStart: '2023/06/29, 19:30 UTC',
+    dateEnd: '2023/06/29, 20:00 UTC',
+    state: 'Active',
+  ),
+  TripItem(
+    tripId: 098292,
+    reserveId: 658137272,
+    user: 'Analía',
+    driver: 'Jorge',
+    total: 68.33,
+    dateStart: '2023/06/30, 07:40 UTC',
+    dateEnd: '2023/06/30, 08:30 UTC',
+    state: 'Active',
+  ),
+  TripItem(
+    tripId: 986392,
+    reserveId: 9875317272,
+    user: 'Andrea',
+    driver: 'Roberto',
+    total: 34.55,
+    dateStart: '2023/06/22, 17:40 UTC',
+    dateEnd: '2023/06/22, 18:30 UTC',
+    state: 'Active',
+  ),
+];
 
 class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final enterpiseGet = ref.watch(getEnterprisesProvider);
-
     final colors = Theme.of(context).colorScheme;
     final screenSize = MediaQuery.of(context).size;
+    const trips = tripItems;
 
     return Scaffold(
       body: Padding(
@@ -61,7 +133,7 @@ class HomeView extends ConsumerWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(15),
                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
                           Text(
                             'Ultimos viajes',
@@ -70,28 +142,158 @@ class HomeView extends ConsumerWidget {
                           ),
                           Column(
                             children: [
-                              Card(
+                              SizedBox(
+                                height: 70,
+                                width: screenSize.width * 0.8,
+                                child: Card(
                                   color: colors.surfaceVariant,
-                                  elevation: 100,
-                                  child: Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          10, 5, 10, 10),
-                                      child: Column(
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Row(
                                         children: [
-                                          Align(
-                                            alignment: Alignment.topRight,
-                                            child: IconButton(
-                                              icon: const Icon(
-                                                  Icons.more_vert_outlined),
-                                              onPressed: () {},
-                                            ),
-                                          ),
-                                          Align(
-                                            alignment: Alignment.bottomLeft,
-                                            child: Text(' - Filled'),
-                                          )
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.info),
+                                          const SizedBox(width: 5),
+                                          Text(
+                                              'Id: ${trips[0].tripId.toString()}'),
+                                          const SizedBox(width: 10),
+                                          const SizedBox(width: 5),
+                                          const Icon(Icons.account_circle),
+                                          Text(
+                                              'Conductor: ${trips[0].driver.toString()}'),
                                         ],
-                                      )))
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.calendar_today),
+                                          Text(
+                                            'Fecha de inicio: ${trips[0].dateStart.toString()}',
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 70,
+                                width: screenSize.width * 0.8,
+                                child: Card(
+                                  color: colors.surfaceVariant,
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.info),
+                                          const SizedBox(width: 5),
+                                          Text(
+                                              'Id: ${trips[1].tripId.toString()}'),
+                                          const SizedBox(width: 10),
+                                          const SizedBox(width: 5),
+                                          const Icon(Icons.account_circle),
+                                          Text(
+                                              'Conductor: ${trips[1].driver.toString()}'),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.calendar_today),
+                                          Text(
+                                            'Fecha de inicio: ${trips[1].dateStart.toString()}',
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 70,
+                                width: screenSize.width * 0.8,
+                                child: Card(
+                                  color: colors.surfaceVariant,
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.info),
+                                          const SizedBox(width: 5),
+                                          Text(
+                                              'Id: ${trips[2].tripId.toString()}'),
+                                          const SizedBox(width: 10),
+                                          const SizedBox(width: 5),
+                                          const Icon(Icons.account_circle),
+                                          Text(
+                                              'Conductor: ${trips[2].driver.toString()}'),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.calendar_today),
+                                          Text(
+                                            'Fecha de inicio: ${trips[2].dateStart.toString()}',
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 70,
+                                width: screenSize.width * 0.8,
+                                child: Card(
+                                  color: colors.surfaceVariant,
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.info),
+                                          const SizedBox(width: 5),
+                                          Text(
+                                              'Id: ${trips[3].tripId.toString()}'),
+                                          const SizedBox(width: 10),
+                                          const SizedBox(width: 5),
+                                          const Icon(Icons.account_circle),
+                                          Text(
+                                              'Conductor: ${trips[3].driver.toString()}'),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          const SizedBox(width: 10),
+                                          const Icon(Icons.calendar_today),
+                                          Text(
+                                            'Fecha de inicio: ${trips[3].dateStart.toString()}',
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
                             ],
                           )
                         ]),
